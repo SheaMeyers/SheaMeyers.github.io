2f1bff7d8d7413784c3382a1facc7fa5476ca6a4
SheaMeyers <shea.meyers@gmail.com>
2019-09-18 19:57:48 +0200

    Start adding basic styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './App.css';
+import photo from './personal_photo.jpeg';
 
 const App: React.FC = () => {
   return (
     <div>
-      This is my personal website
+      <div className="Top-bar">
+        <a className="Link" href="https://www.linkedin.com/in/sheameyers/">LinkedIn</a>
+        <a className="Link" href="https://github.com/SheaMeyers">Github</a>
+      </div>
+      <img src={photo} alt="logo" />
+      <div>
+        This is my personal website
+      </div>
     </div>
   );
 };

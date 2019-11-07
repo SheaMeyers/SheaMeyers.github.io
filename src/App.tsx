2cb256e91327820035bb8cbd4d79c1bdd69fb044
SheaMeyers <shea.meyers@gmail.com>
2019-11-07 07:38:28 +0100

    Styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,27 @@ import './App.css';
 import photo from './personal_photo.jpeg';
 
 const App: React.FC = () => {
-  return (
-    <div className="App-body">
-      <div className="Header-bar">
-          <div className="Header-bar-div">
-            <a className="Link" href="https://www.linkedin.com/in/sheameyers/">LinkedIn</a>
-          </div>
-          <div className="Header-bar-div">
-              <a className="Link" href="https://github.com/SheaMeyers">Github</a>
-          </div>
-      </div>
-      <div className="Main-body">
-          <img className="Photo" src={photo} alt="logo" />
-          <div className="Main-body-text">
-              <h3>About Me</h3>
-              <p>Hello, my name is Shea Meyers and I am a full stack web developer</p>
-              <p>My programming interested are primarily related to ReactJS, NodeJS, and Python/Django</p>
-              <p>Born and raised in Canada I now reside in The Netherlands</p>
-          </div>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App-body">
+            <div className="Header-bar">
+                <div className="Header-bar-div">
+                    <a className="Link" href="https://www.linkedin.com/in/sheameyers/">LinkedIn</a>
+                </div>
+                <div className="Header-bar-div">
+                    <a className="Link" href="https://github.com/SheaMeyers">Github</a>
+                </div>
+            </div>
+            <div className="Main-body">
+                <img className="Photo" src={photo} alt="logo"/>
+                <div className="Main-body-text">
+                    <h3>About Me</h3>
+                    <p>Hello, my name is Shea Meyers and I am a full stack web developer</p>
+                    <p>My programming interested are primarily related to ReactJS, NodeJS, and Python/Django</p>
+                    <p>Born and raised in Canada I now reside in The Netherlands</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default App;

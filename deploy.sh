f337dd9dc52b3824cf4caf37cc1a592e34c7d889
SheaMeyers <shea.meyers@gmail.com>
2019-12-24 20:09:05 +0100

    Add gcloud commands to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,13 @@
 # Deploy script for deploying to GKE
 
+gcloud components update kubectl --no-input
+
+gcloud config set project shea-meyers-personal-website
+
+gcloud config set compute/zone europe-west4-b
+
+gcloud container clusters get-credentials personal-website-cluster
+
 SHA=$(git rev-parse HEAD)
 
 docker build -t eu.gcr.io/shea-meyers-personal-website/personal-website:latest -t eu.gcr.io/shea-meyers-personal-website/personal-website:$SHA .

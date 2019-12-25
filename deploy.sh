cc062dcce928a18fde2cf7c11d56fc3fa662fa33
SheaMeyers <shea.meyers@gmail.com>
2019-12-25 14:33:26 +0100

    Update deploy script

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 gcloud components update kubectl
 
-gcloud config set project shea-meyers-personal-website
+gcloud config set project personal-website-id
 
 gcloud config set compute/zone europe-west4-b
 
@@ -10,12 +10,12 @@ gcloud container clusters get-credentials personal-website-cluster
 
 SHA=$(git rev-parse HEAD)
 
-docker build -t eu.gcr.io/shea-meyers-personal-website/personal-website:latest -t eu.gcr.io/shea-meyers-personal-website/personal-website:$SHA .
+docker build -t eu.gcr.io/personal-website-id/personal-website:latest -t eu.gcr.io/personal-website-id/personal-website:$SHA .
 
-docker push eu.gcr.io/shea-meyers-personal-website/personal-website:latest
+docker push eu.gcr.io/personal-website-id/personal-website:latest
 
-docker push eu.gcr.io/shea-meyers-personal-website/personal-website:$SHA
+docker push eu.gcr.io/personal-website-id/personal-website:$SHA
 
 kubectl apply -f ../personal-website-k8s
 
-kubectl set image deployments/personal-website-deployment react=eu.gcr.io/shea-meyers-personal-website/personal-website:$SHA
+kubectl set image deployments/personal-website-deployment react=eu.gcr.io/personal-website-id/personal-website:$SHA

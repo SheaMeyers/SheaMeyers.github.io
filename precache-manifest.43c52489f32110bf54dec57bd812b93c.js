a43b33a3c32bcfa5c5991911cacb3a69f26e82f4
SheaMeyers <shea.meyers@gmail.com>
2019-12-31 21:18:13 +0100

    Updates

---
+++ precache-manifest.43c52489f32110bf54dec57bd812b93c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc0545e2535483c7746ad8c1635d35ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9fe5fee73f4bf7f06fb2",
+    "url": "/static/css/main.169b8858.chunk.css"
+  },
+  {
+    "revision": "510aeafd5b31554d6cc6",
+    "url": "/static/js/2.5f8f5a35.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5f8f5a35.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9fe5fee73f4bf7f06fb2",
+    "url": "/static/js/main.b1731ab8.chunk.js"
+  },
+  {
+    "revision": "b48494abca701cfe4673",
+    "url": "/static/js/runtime-main.6271eae4.js"
+  }
+]);

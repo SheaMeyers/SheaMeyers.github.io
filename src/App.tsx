1ae95d4094678909f0425b3c1a4884ef4009d620
Shea Meyers <shea.meyers@gmail.com>
2020-03-05 07:26:43 +0100

    Update layout of App

--- src/App.tsx
+++ src/App.tsx
@@ -6,22 +6,29 @@ const App: React.FC = () => {
     return (
         <div className="App-body">
             <div className="Header-bar">
-                <div className="Header-bar-div">
+                <div className="Header-bar--link-div">
                     <a className="Link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sheameyers/">LinkedIn</a>
                 </div>
-                <div className="Header-bar-div">
+                <div className="Header-bar--link-div">
                     <a className="Link" target="_blank" rel="noopener noreferrer" href="https://github.com/SheaMeyers">Github</a>
                 </div>
             </div>
-            <div className="Main-body">
-                <img className="Photo" src={photo} alt="logo"/>
-                <div className="Main-body-text">
-                    <h3>About Me</h3>
-                    <p>Hello, my name is Shea Meyers and I am a full stack web developer</p>
-                    <p>My programming interests are primarily related to ReactJS, NodeJS, and Python/Django</p>
-                    <p>I'm also interested in Docker and Kubernetes (this site is hosted using Google Kubernetes Engine)</p>
-                    <p>Born and raised in Canada I now reside in The Netherlands</p>
+            <div className="Quick-summary">
+                <img className="Quick-summary--photo" src={photo} alt="logo"/>
+                <div className="Quick-summary--text">
+                    <h1 className="Quick-summary--header">
+                        Hello, my name is <span className="Quick-summary--highlight">Shea Meyers</span>
+                    </h1>
+                    <h1 className="Quick-summary--header">
+                        and I am a <span className="Quick-summary--highlight">Full Stack Web Developer</span>
+                    </h1>
                 </div>
+            </div>
+            <div className="Bottom-portion">
+                    <h3>Quick About Me</h3>
+                    <p>My programming interests are primarily related to ReactJS, NodeJS, and Python/Django</p>
+                    <p>I'm also a big fan of in Docker and Kubernetes</p>
+                    <p>Born and raised in Canada I now reside in The Netherlands</p>
             </div>
         </div>
     );

9178c190e7e61c2ebff87bdf992e81e74eed104d
Shea Meyers <shea.meyers@gmail.com>
2020-03-07 12:32:08 +0100

    Use react router and restructure app

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,18 @@
 import React from 'react';
-import './App.css';
-import photo from './personal_photo.jpeg';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from './Home';
 
 const App: React.FC = () => {
     return (
-        <div className="App-body">
-            <div className="Header-bar">
-                <div>
-                    About Me
-                </div>
-                <div className="Header-bar--left">
-                    <div className="Header-bar--link-div">
-                        <a className="Link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sheameyers/">LinkedIn</a>
-                    </div>
-                    <div className="Header-bar--link-div">
-                        <a className="Link" target="_blank" rel="noopener noreferrer" href="https://github.com/SheaMeyers">Github</a>
-                    </div>
-                </div>
-            </div>
-            <div className="Quick-summary">
-                <img className="Quick-summary--photo" src={photo} alt="logo"/>
-                <div className="Quick-summary--text">
-                    <h1 className="Quick-summary--header">
-                        Hello, my name is <span className="Quick-summary--highlight">Shea Meyers</span>
-                    </h1>
-                    <h1 className="Quick-summary--header">
-                        and I am a <span className="Quick-summary--highlight">Full Stack Web Developer</span>
-                    </h1>
-                </div>
-            </div>
-            <div className="Bottom-portion">
-                    <p className="Bottom-portion--text">My programming interests are primarily related to ReactJS, NodeJS, and Python/Django</p>
-                    <p className="Bottom-portion--text">I'm also a big fan of in Docker and Kubernetes</p>
-                    <p className="Bottom-portion--text">Born and raised in Canada I now reside in The Netherlands</p>
-            </div>
-        </div>
-    );
-};
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={Home} exact={true} />
+                {/* <Route path="/about-me" component={AboutMe}/> */}
+                {/* <Route path="/projects" component={Projects}/> */}
+                {/* <Route component={NotFoundPage}/>  */}
+            </Switch>
+        </BrowserRouter>
+    )
+}
 
 export default App;
---
+++ src/Home.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './Home.css';
+import photo from './personal_photo.jpeg';
+
+const Home: React.FC = () => {
+    return (
+        <div className="Home-body">
+            <div className="Header-bar">
+                <div>
+                    About Me
+                </div>
+                <div className="Header-bar--left">
+                    <div className="Header-bar--link-div">
+                        <a className="Link" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sheameyers/">LinkedIn</a>
+                    </div>
+                    <div className="Header-bar--link-div">
+                        <a className="Link" target="_blank" rel="noopener noreferrer" href="https://github.com/SheaMeyers">Github</a>
+                    </div>
+                </div>
+            </div>
+            <div className="Quick-summary">
+                <img className="Quick-summary--photo" src={photo} alt="logo"/>
+                <div className="Quick-summary--text">
+                    <h1 className="Quick-summary--header">
+                        Hello, my name is <span className="Quick-summary--highlight">Shea Meyers</span>
+                    </h1>
+                    <h1 className="Quick-summary--header">
+                        and I am a <span className="Quick-summary--highlight">Full Stack Web Developer</span>
+                    </h1>
+                </div>
+            </div>
+            <div className="Bottom-portion">
+                    <p className="Bottom-portion--text">My programming interests are primarily related to ReactJS, NodeJS, and Python/Django</p>
+                    <p className="Bottom-portion--text">I'm also a big fan of in Docker and Kubernetes</p>
+                    <p className="Bottom-portion--text">Born and raised in Canada I now reside in The Netherlands</p>
+            </div>
+        </div>
+    );
+};
+
+export default Home;

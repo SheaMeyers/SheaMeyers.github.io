06c14654d6c3f05781ac9969d88237e2107d4760
Shea Meyers <shea.meyers@gmail.com>
2020-07-16 20:53:01 +0200

    Fix header bar linking

--- src/HeaderBar.tsx
+++ src/HeaderBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
 import './HeaderBar.css';
 
@@ -11,9 +11,9 @@ const HeaderBar: React.FC = () => {
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                { pathname !== '/' &&  <Nav.Link href="/">Home</Nav.Link> }
-                { pathname !== '/about-me' && <Nav.Link href="/about-me">About Me</Nav.Link> }
-                { pathname !== '/projects' && <Nav.Link href="/projects">Projects</Nav.Link> }
+                { pathname !== '/' &&  <Link className="nav-link" to={'/'}>Home</Link> }
+                { pathname !== '/about-me' && <Link className="nav-link" to={'/about-me'}>About Me</Link> }
+                { pathname !== '/projects' && <Link className="nav-link" to={'/projects'}>Projects</Link> }
                 <Nav.Link target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/sheameyers/">LinkedIn</Nav.Link>
                 <Nav.Link target="_blank" rel="noopener noreferrer" href="https://github.com/SheaMeyers">Github</Nav.Link>
                 </Nav>

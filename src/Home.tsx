3128543e281491369b3fbcffd369b29d6da3ba6b
Shea Meyers <shea.meyers@gmail.com>
2020-11-29 20:31:42 +0100

    Show text over picture

--- src/Home.tsx
+++ src/Home.tsx
@@ -7,8 +7,10 @@ const Home: React.FC = () => {
     return (
         <div className="Home-body">
             <HeaderBar />
-            <div>
+            <div className="Main-portion">
                 <img className="Photo" src={photo} alt="Shea Meyers" title="Shea Meyers"/>
+                <h1 className="Main-portion--name">Shea Meyers</h1>
+                <h2 className="Main-portion--job">Fullstack Web Developer</h2>
             </div>
             <div className="Bottom-portion">
                 <p className="Bottom-portion--text">I'm mostly experienced in ReactJS and Django</p>

2545e5e31d16ac416bddc5c478e4ad2e09373085
Shea Meyers <shea.meyers@gmail.com>
2020-12-08 20:17:21 +0100

    Updates

---
+++ precache-manifest.24f92bee9579b8895bbccb120ae8b7b8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce3880838fe713be97bc11b8ce667cb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b557361cd375dea2417",
+    "url": "/static/css/2.43079d8c.chunk.css"
+  },
+  {
+    "revision": "20f636591d72e5ec16e2",
+    "url": "/static/css/main.9ce785ae.chunk.css"
+  },
+  {
+    "revision": "0b557361cd375dea2417",
+    "url": "/static/js/2.f54ed2e7.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.f54ed2e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20f636591d72e5ec16e2",
+    "url": "/static/js/main.97b989f6.chunk.js"
+  },
+  {
+    "revision": "ea6af7b72f13156a2c8d",
+    "url": "/static/js/runtime-main.bcb74886.js"
+  },
+  {
+    "revision": "beccb1106f1a6636d4598c4f45c17a01",
+    "url": "/static/media/HomePhoto.beccb110.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5fc1527471d045559a143ae63ed9d714.js"
+  "/precache-manifest.24f92bee9579b8895bbccb120ae8b7b8.js"
 );
 
 self.addEventListener('message', (event) => {

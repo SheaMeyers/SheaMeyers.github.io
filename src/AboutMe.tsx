8ed739bb9a7394f5ffd239d68b9f91269c2227f6
Shea Meyers <shea.meyers@gmail.com>
2021-03-16 20:32:08 +0100

    Move text to center of the page

--- src/AboutMe.tsx
+++ src/AboutMe.tsx
@@ -10,100 +10,73 @@ const AboutMe: React.FC = () => {
                 <h2>A Quick Overview</h2>
             </div>
             <div className="About-me-non-white-section">
-                <p className='About-me-sentence'>I was born and raised in a small town in Saskatchewan in a family of 5</p>
-                <p className='About-me-sentence'>After high school I went to the University of Saskatchewan to study engineering</p>
-                <p className='About-me-sentence'>I decided that engineering was not for me after a year of study and 
-                eventually decided on studying computer science
+                <p className="About-me-section__text">
+                    I was born and raised in a small town in Saskatchewan in a family of 5.
+                    After high school I went to the University of Saskatchewan to study engineering.
+                    I decided that engineering was not for me after a year of study and eventually 
+                    decided on studying computer science.
                 </p>
             </div>
             <div className="About-me-section">
-                <p className='About-me-sentence'>It was then that I discovered my love of programming and also discovered my love of travel</p>
-                <p className='About-me-sentence'>
+                <p className="About-me-section__text">
+                    It was then that I discovered my love of programming and also discovered my love of travel.
                     I became friends with a few German exchange students and ended up spending Christmas break
-                    one year in Germany
-                </p>
-                <p className='About-me-sentence'>
+                    one year in Germany.
                     This time made me realise how many different lives and cultures existed in the world and 
-                    I decided I would live abroad as soon as I could
+                    I decided I would live abroad as soon as I could.
                 </p>
             </div>
             <div className="About-me-non-white-section">
-                <p className='About-me-sentence'>It was also in university that I met the love of my life and future wife Hailey</p>
-                <p className='About-me-sentence'>
+                <p className="About-me-section__text">
+                    It was also in university that I met the love of my life and future wife Hailey.
                     Between my second and third year I met my wife while out with a group of friends and
-                    we have been together ever since
+                    we have been together ever since.
                 </p>
             </div>
             <div className="About-me-section">
-                <p className='About-me-sentence'>
-                    My first "real" job was a year long intership I did at a company called Sasktel between my third and 
-                    fourth year of university
-                </p>
-                <p className='About-me-sentence'>
-                    While the money was good it made me realize how much I disliked corporate cubicle work environments
-                    and have vouwed never to work in an environment like that again
+                <p className="About-me-section__text">
+                    My first "real" job was a year long intership I did at a company called Sasktel 
+                    between my third and fourth year of university.
+                    While the money was good it made me realize how much I disliked corporate cubicle 
+                    work environments and have always worked in a more casual work environment since then.
                 </p>
             </div>
             <div className="About-me-non-white-section">
-                <p className='About-me-sentence'>
+                <p className="About-me-section__text">
                     After completing my internship and final year of university 
-                    I got a job working in my local city at a company called Vendasta
-                </p>
-                <p className='About-me-sentence'>
+                    I got a job working in my local city at a company called Vendasta.             
                     I worked at Vendasta for a year and half and
-                    it gave me a great foundation of knowledge that I've continued to use
+                    it gave me a great foundation of knowledge that I've continued to use.
                 </p>
             </div>
             <div className="About-me-section">
-                <p className='About-me-sentence'>
-                    While I enjoyed working at Vendasta I always kept in mind my dream of living abroad
-                </p>
-                <p className='About-me-sentence'>
-                    I worked at Vendasta for a eighteen months and decided I had enough industry experience and money saved up 
-                    so I began looking for jobs in The Netherlands
+                <p className="About-me-section__text">
+                    While I enjoyed working at Vendasta I always kept in mind my dream of living abroad.
+                    I worked at Vendasta for a eighteen months and decided I had enough industry experience 
+                    and money saved up so I began looking for jobs in The Netherlands.
                 </p>
             </div>
             <div className="About-me-non-white-section">
-                <p className='About-me-sentence'>
+                <p className="About-me-section__text">
                     After many job applications and a few interviews I received a job working with the company Calendar42,  
                     which would eventually be rebranded to Toogethr.
-                </p>
-                <p className='About-me-sentence'>
                     I worked with Toogethr for three and half years and decided I wanted a new challenge and got a 
                     new job where I am currently working, Maykin Media
                 </p>
             </div>
             <div className="About-me-section">
-                <p className='About-me-sentence'>
-                    One of the best benefits to living in the Netherlands was the easy ability to travel
-                </p>
-                <p className='About-me-sentence'>
-                    Since living here my wife and I have been able to visit 13 countries
-                </p>
-                <p className='About-me-sentence'>
+                <p className="About-me-section__text">
+                    One of the best benefits to living in the Netherlands was the easy ability to travel.
+                    Since living here my wife and I have been able to visit 13 countries.
                     Belgium, France, Germany, Italy, Greece, Spain, Portugal, 
-                    England, Ireland, Scotland, Norway, Sweden, and Denmark
+                    England, Ireland, Scotland, Norway, Sweden, and Denmark.
                 </p>
             </div>
             <div className="About-me-non-white-section">
-                <p className='About-me-sentence'>
-                    For now I simply continue to enjoy life
-                </p>
-            </div>
-            <div className="About-me-section">
-                <p className='About-me-sentence'>
-                    Professionally I'm always interested in learning and develop new skills
-                </p>
-            </div>
-            <div className="About-me-non-white-section">
-                <p className='About-me-sentence'>
-                    Personally I continue to integrate into Dutch culture and while 
-                </p>
-                <p className='About-me-sentence'>
+                <p className="About-me-section__text">
+                    I'm continuing to integrate into Dutch culture and while 
                     I've passed both the inburgerings (integration) exams and staatsexams (state exams)
-                </p>
-                <p className='About-me-sentence'>
-                    I still feel there is much to learn about both the language and culture
+                    I still feel there is much to learn about both the language and culture.
                 </p>
             </div>
         </div>
--- src/Now.tsx
+++ src/Now.tsx
@@ -16,31 +16,33 @@ const Now: React.FC = () => {
                     <h2 className="Now--header">What am I doing now?</h2>
                 </div>
                 <div className="Now--white-section">
-                    <h3 className="Now--sub-header">
-                        I try to keep this page current about what I'm doing at the moment
-                    </h3>
-                    <h4 className="Left-padding">So, what I'm doing:</h4>
-                    <ul>
-                        <li>
-                            Learning Dutch.  I do this by reading books, watching TV, and writing.  
-                            I also try to speak Dutch with people whenever possible but with the current
-                            covid situation this is difficult
-                        </li>
-                        <li>
-                            Projects.  On my home page you'll see two other websites.
-                            I've programmed these and now I'm studying marketing 
-                            to try to get people to know about these
-                        </li>
-                        <li>
-                            Gaming.  My wife and I decided to get a Nintendo Switch for Christmas so I've been
-                            playing a lot of video games, especially "The Legend of Zelda: Breath of the Wild"
-                        </li>
-                    </ul>
-                    <p className="Left-padding">
-                        Otherwise I spend my free time however I want to.  
-                        This includes spending time with my wife, cat, and friends, exercising, reading,
-                        watching Netflix, and anything else that I decide I want to do. 
-                    </p>
+                    <div className="Now--margins">
+                        <h3 className="Now--sub-header">
+                            I try to keep this page current about what I'm doing at the moment
+                        </h3>
+                        <h4 className="Left-padding">So, what I'm doing:</h4>
+                        <ul>
+                            <li>
+                                Learning Dutch.  I do this by reading books, watching TV, and writing.  
+                                I also try to speak Dutch with people whenever possible but with the current
+                                covid situation this is difficult
+                            </li>
+                            <li>
+                                Projects.  On my home page you'll see two other websites.
+                                I've programmed these and now I'm studying marketing 
+                                to try to get people to know about these
+                            </li>
+                            <li>
+                                Gaming.  My wife and I decided to get a Nintendo Switch for Christmas so I've been
+                                playing a lot of video games, especially "The Legend of Zelda: Breath of the Wild"
+                            </li>
+                        </ul>
+                        <p className="Left-padding">
+                            Otherwise I spend my free time however I want to.  
+                            This includes spending time with my wife, cat, and friends, exercising, reading,
+                            watching Netflix, and anything else that I decide I want to do. 
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>

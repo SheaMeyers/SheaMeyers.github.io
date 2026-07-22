02d164fe0567a237f14705868672ed2f8b5d5216
SheaMeyers <shea.meyers@gmail.com>
2026-07-22 20:36:32 +0200

    Update my now page

--- src/Now.tsx
+++ src/Now.tsx
@@ -9,7 +9,7 @@ const Now: React.FC = () => {
             <div className="Now--white-section Now--hero-section">
                 <div className="Now--hero-content">
                     <a href="https://nownownow.com/p/dYg2" target="_blank" rel="noopener noreferrer">This is my now page!</a>
-                    <p>Last updated: 26 April 2026</p>
+                    <p>Last updated: 22 July 2026</p>
                 </div>
             </div>
             <div className="Now--non-white-section">
@@ -24,28 +24,23 @@ const Now: React.FC = () => {
                     <ul>
                         <li>
                             Baby Prep!
-                            My wife is recently pregnant so we are doing all we can to prepare.
+                            My wife is pregnant so we are doing all we can to prepare.
                             Thankfully we have good friends who have given us a lot of second hand stuff that we are so grateful to get.
                         </li>
                         <li>
-                            Learning Dutch:  I do this by reading books, watching TV, and writing.  
-                            I also try to speak Dutch whenever possible.  Right now I'm reading
-                            The Witcher series (in Dutch), watching the Dutch TV show Dertigers, and translating 
-                            "The Trial" by Franz Kafka from English into Dutch.
+                            Programming:  I just built new projects  
+                            <a style={{margin: '0.3rem'}} href="https://dutchpractice.com/" target="_blank" rel="noopener noreferrer">Dutch Practice</a>
+                            and <a style={{margin: '0.3rem'}} href="https://translatedstories.com/" target="_blank" rel="noopener noreferrer">Translated Stories</a> <br />
                         </li>
                         <li>
-                            Programming:  I just built a new project at 
-                            <a style={{margin: '0.3rem'}} href="https://dutchpractice.com/" target="_blank" rel="noopener noreferrer">Dutch Practice</a> <br />
-                            I'm also planning to add more activities to the website.
-                        </li>
-                        <li>
-                            Gaming:  At the moment I'm playing The Witcher #3: Wild Hunt.
-                            I've beaten it, including the expansions, once before but I found it so fun I'm playing it again
+                            Reading:  I've been getting into reading more lately and have been reading the classical famous books.  Most recently 
+                            I finished reading Crime & Punishment and The Trial, both which I very much enjoyed for different reasons.  Now I'm reading
+                            A Tale of Two Cities and afterwards plan to read The Great Gasby and Oliver Twist.
                         </li>
                     </ul>
                     <p className="Left-padding">
                         Otherwise I spend my free time however I want to.  
-                        This includes spending time with my wife, cat, and friends, exercising, reading,
+                        This includes spending time with my wife, cat, and friends, exercising, gaming,
                         watching TV, and anything else that I decide I want to do. 
                     </p>
                 </div>
